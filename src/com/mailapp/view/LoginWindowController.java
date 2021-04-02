d4898126b0ffc6b4072d9b9eb7080dcd662e9abf
Maciej Anioł <maciej.aniol.programista@gmail.com>
2021-04-02 18:00:53 +0200

    feat: Login window added.

---
+++ src/com/mailapp/view/LoginWindowController.java
@@ -0,0 +1,27 @@
+package com.mailapp.view;
+
+        import javafx.fxml.FXML;
+        import javafx.scene.control.Button;
+        import javafx.scene.control.Label;
+        import javafx.scene.control.TextField;
+
+public class LoginWindowController {
+
+    @FXML
+    private TextField passwordField;
+
+    @FXML
+    private TextField emailAddressField;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Label errorText;
+
+    @FXML
+    void loginButonAction() {
+
+    }
+
+}

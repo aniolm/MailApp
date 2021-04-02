e8e317cf365ba31be22c59d77a93e62a4ac0c86d
Maciej Anioł <maciej.aniol.programista@gmail.com>
2021-04-02 17:07:27 +0200

    Initial commit

---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+module MailApp {
+
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.web;
+
+    opens com.mailapp;
+    opens com.mailapp.view;
+}

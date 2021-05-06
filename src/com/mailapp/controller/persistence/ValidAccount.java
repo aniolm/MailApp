956240204faf36e0a108a6ac16809245d704ebdb
Maciej Anioł <maciej.aniol.programista@gmail.com>
2021-05-06 20:19:52 +0200

    Feat: Application persistence added.

---
+++ src/com/mailapp/controller/persistence/ValidAccount.java
@@ -0,0 +1,31 @@
+package com.mailapp.controller.persistence;
+
+import java.io.Serializable;
+
+public class ValidAccount implements Serializable {
+
+    public ValidAccount(String address, String password) {
+        this.address = address;
+        this.password = password;
+    }
+
+    private String address;
+    private String password;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}

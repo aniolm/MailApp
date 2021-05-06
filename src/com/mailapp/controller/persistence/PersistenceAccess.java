f55fb06c15b5ff21a2312cec91ea8b87c5158734
Maciej Anioł <maciej.aniol.programista@gmail.com>
2021-05-06 20:47:09 +0200

    feat: Password encryption added.

--- src/com/mailapp/controller/persistence/PersistenceAccess.java
+++ src/com/mailapp/controller/persistence/PersistenceAccess.java
@@ -2,10 +2,12 @@ package com.mailapp.controller.persistence;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 
 public class PersistenceAccess {
     private String VALID_ACCOUNTS_lOCATION = System.getProperty("user.home") + File.separator + "validAccounts.ser";
+    private Encryptor encryptor = new Encryptor();
 
     public List<ValidAccount> loadFromPersistence(){
         List<ValidAccount> resultList = new ArrayList<ValidAccount>();
@@ -13,6 +15,7 @@ public class PersistenceAccess {
             FileInputStream fileInputStream = new FileInputStream(VALID_ACCOUNTS_lOCATION);
             ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
             List<ValidAccount> persistedList = (List<ValidAccount>) objectInputStream.readObject();
+            decryptPasswords(persistedList);
             resultList.addAll(persistedList);
 
         } catch (Exception e) {
@@ -20,11 +23,26 @@ public class PersistenceAccess {
         }
         return resultList;
     }
+
+    private void decryptPasswords(List<ValidAccount> persistedList) {
+        for (ValidAccount validAccount: persistedList){
+            String originalPassword = validAccount.getPassword();
+            validAccount.setPassword(encryptor.decrypt(originalPassword));
+        }
+    }
+
+    private void encryptPasswords(List<ValidAccount> persistedList) {
+        for (ValidAccount validAccount: persistedList){
+            String originalPassword = validAccount.getPassword();
+            validAccount.setPassword(encryptor.encrypt(originalPassword));
+        }
+    }
     public void saveToPersistence(List<ValidAccount> validAccounts){
         try {
             File file = new File(VALID_ACCOUNTS_lOCATION);
             FileOutputStream fileOutputStream = new FileOutputStream(file);
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            encryptPasswords(validAccounts);
             objectOutputStream.writeObject(validAccounts);
             objectOutputStream.close();
             fileOutputStream.close();

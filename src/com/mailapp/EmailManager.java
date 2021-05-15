06ef73cc78d1b63ce56bdbfc1d157fc425f349f2
Maciej Anioł <maciej.aniol.programista@gmail.com>
2021-05-15 06:33:40 +0200

    feat: Folder icons added.

--- src/com/mailapp/EmailManager.java
+++ src/com/mailapp/EmailManager.java
@@ -5,6 +5,7 @@ import com.mailapp.controller.services.FolderUpdaterService;
 import com.mailapp.model.EmailAccount;
 import com.mailapp.model.EmailMessage;
 import com.mailapp.model.EmailTreeItem;
+import com.mailapp.view.IconResolver;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.TreeItem;
@@ -22,6 +23,12 @@ public class EmailManager {
     private EmailMessage selectedMessage;
     private EmailTreeItem selectedFolder;
     private ObservableList<EmailAccount> emailAccounts = FXCollections.observableArrayList();
+    private IconResolver iconResolver = new IconResolver();
+
+    public EmailManager(){
+        folderUpdaterService = new FolderUpdaterService(folderList);
+        folderUpdaterService.start();
+    }
 
     public ObservableList<EmailAccount> getEmailAccounts() {
         return emailAccounts;
@@ -43,11 +50,6 @@ public class EmailManager {
         return selectedFolder;
     }
 
-    public EmailManager(){
-        folderUpdaterService = new FolderUpdaterService(folderList);
-        folderUpdaterService.start();
-    }
-
     public List<Folder> getFolderList() {
         return this.folderList;
     }
@@ -59,6 +61,7 @@ public class EmailManager {
     public void addEmailAccount(EmailAccount emailAccount){
         emailAccounts.add(emailAccount);
         EmailTreeItem<String> treeItem = new EmailTreeItem<String>(emailAccount.getAddress());
+        treeItem.setGraphic(iconResolver.getIconForFolder(emailAccount.getAddress()));
         FetchFoldersService fetchFoldersService = new FetchFoldersService(emailAccount.getStore(), treeItem, folderList);
         fetchFoldersService.start();
         foldersRoot.getChildren().add(treeItem);
--- src/com/mailapp/controller/services/FetchFoldersService.java
+++ src/com/mailapp/controller/services/FetchFoldersService.java
@@ -1,6 +1,7 @@
 package com.mailapp.controller.services;
 
 import com.mailapp.model.EmailTreeItem;
+import com.mailapp.view.IconResolver;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 
@@ -17,6 +18,7 @@ public class FetchFoldersService extends Service<Void> {
     private Store store;
     private EmailTreeItem<String> foldersRoot;
     private List<Folder> folderList;
+    private IconResolver iconResolver = new IconResolver();
 
     public FetchFoldersService(Store store,EmailTreeItem<String> foldersRoot, List<Folder> folderList) {
         this.store = store;
@@ -45,6 +47,7 @@ public class FetchFoldersService extends Service<Void> {
         for(Folder folder: folders) {
             folderList.add(folder);
             EmailTreeItem<String> emailTreeItem = new EmailTreeItem<String>(folder.getName());
+            emailTreeItem.setGraphic(iconResolver.getIconForFolder(folder.getName()));
             foldersRoot.getChildren().add(emailTreeItem);
             foldersRoot.setExpanded(true);
             fetchMessagesInFolder(folder, emailTreeItem);
